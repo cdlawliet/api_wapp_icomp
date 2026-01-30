55e2dcf65b11cf20df3431b2e54e282e31f1623d
cdlawliet <caio_lawliet@hotmail.com>
2026-01-30 17:02:18 -0300

    primeira versão webapp icompany

--- whatsappClient.js
+++ whatsappClient.js
@@ -13,7 +13,7 @@ async function createClient() {
     authStrategy: new LocalAuth({ clientId: 'bot-zdg' }),
     puppeteer: {
       executablePath,
-      headless: false,
+      headless: true,
       args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
